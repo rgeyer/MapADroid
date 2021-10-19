0f195997c777e39d12a0a0dee954c82cacab83b8
Ryan J. Geyer <me@ryangeyer.com>
2021-10-19 10:20:46 -0700

    Make the nuclear pokemon clearing option a little softer, keep an hour of history

--- src/bash/purge-and-optimize-pokemon-tbl.sh
+++ src/bash/purge-and-optimize-pokemon-tbl.sh
@@ -3,7 +3,13 @@ if [[ -z "$DBHOST" || -z "$DBNAME" || -z "$DBUSER" ]]; then
     echo "DBHOST, DBNAME and DBUSER Environment variables must be set".
     exit 1
 fi
-mysqldump -h $DBHOST -d -u $DBUSER -p $DBNAME pokemon > pokemon.sql
-echo "optimize table pokemon;" >> pokemon.sql
-mysql -h $DBHOST -u $DBUSER -p $DBNAME < pokemon.sql
-rm -rf pokemon.sql
+
+read -r -d '' query <<- EOF
+drop table pokemon_old;
+create table pokemon_purge like pokemon;
+insert into pokemon_purge select * from pokemon where last_modified > now() - interval 1 hour;
+rename table pokemon to pokemon_old;
+rename table pokemon_purge to pokemon;
+EOF
+
+mysql -h $DBHOST -u $DBUSER -p $DBNAME -e "$query"
